e75b0e10c60898bf388de39e318de2a82ba3336f
rpiccoline1 <rpiccoline@hotmail.com>
2018-05-03 18:18:36 -0400

    gave up on modal

--- public/app.js
+++ public/app.js
@@ -1,38 +1,38 @@
 $.getJSON("/articles", function (data) {
-  
+
   for (var i = 0; i < data.length; i++) {
-    
+
     $("#article-space").append("<h3 data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</h3>");
   }
 });
 
 $(document).on("click", "h3", function () {
- console.log("test");
-  $(".modal-body").empty();
-  
+  console.log("test");
+  $("#comment-space").empty();
+
   var thisId = $(this).attr("data-id");
-console.log(thisId);
+  console.log(thisId);
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
   })
     .then(function (data) {
       console.log(data);
-      
-      $(".modal-body").append("<h2>" + data.title + "</h2>");
-      $(".modal-body").append("<input id='titleinput' name='title' >");
-      $(".modal-body").append("<textarea id='bodyinput' name='body'></textarea>");
-      $(".modal-body").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-      if (data.note) {
+      $("#comment-space").append("<h3>" + data.title + "</h3>");
+      $("#comment-space").append("<input id='titleinput' name='title' >");
+      $("#comment-space").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#comment-space").append("<button data-id='" + data._id + "' id='savecomment'>Save Comment</button>");
 
-        $("#titleinput").val(data.note.title);
-        $("#bodyinput").val(data.note.body);
+      if (data.comment) {
+
+        $("#titleinput").val(data.comment.title);
+        $("#bodyinput").val(data.comment.body);
       }
     });
 });
 
-$(document).on("click", "#savenote", function () {
+$(document).on("click", "#savecomment", function () {
 
   var thisId = $(this).attr("data-id");
 
@@ -40,17 +40,17 @@ $(document).on("click", "#savenote", function () {
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-    
+
       title: $("#titleinput").val(),
       body: $("#bodyinput").val()
     }
   })
-    
+
     .then(function (data) {
-  
+
       console.log(data);
-    
-      $("#notes").empty();
+
+      $("#comment-space").empty();
     });
 
   $("#titleinput").val("");
